a6eeef9ac856b7964d8052f9459781e1c887ebe3
WezQa <107553409+WezQa@users.noreply.github.com>
2022-08-15 23:06:12 +0100

    Add files via upload

--- src/main/java/com/qa/repo/UserRepo.java
+++ src/main/java/com/qa/repo/UserRepo.java
@@ -1,15 +1,19 @@
 package com.qa.repo;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.qa.entity.User;
 
 @Repository
-public interface UserRepo extends JpaRepository<User, Long> {
+public interface UserRepo extends JpaRepository<User, String> {
 
-	Optional<User> findByName(String Name);
+	@Query(value = "SELECT * FROM User", nativeQuery = true)
+	public List<User> allFromUser();
+
+	public Object findAllById(Long testId);
 
 }
